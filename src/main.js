4f794c2db00ab9b4d3269918626676f623d0bb4f
realib1 <ibeevpersie@gmail.com>
2023-03-29 17:37:48 -0600

    Still working

--- src/main.js
+++ src/main.js
@@ -1,30 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navSlide = () => {
-        const burger = document.querySelector('.burger');
-        const nav = document.querySelector('.nav-links');
-        const navLinks = document.querySelectorAll('.nav-links li');
-    
-        burger.addEventListener('click', () => {
-            //Toggle Nav
-            nav.classList.toggle('nav-active');
-    
-            //Animate Links
-            navLinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = '';
-                    console.log('remove animation');
-                } else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-                    console.log('add animation');
-                }
-            });
-            //Burger animation
-            burger.classList.toggle('toggle');
-        });
-    }
-    
-    navSlide()
+window.addEventListener('DOMContentLoaded', () => {
+
+// Responsive Navbar
+const menu = document.querySelector('.menu');
+const bars = document.querySelector('.bars');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+
+bars.addEventListener('click', () => {
+    menu.classList.toggle('hidden');    
     });
 
 
-    
+
+// Scroll to top button
+// const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
+
+// window.addEventListener('scroll', () => {
+//     if (window.pageYOffset > 100) {
+//         scrollToTopBtn.classList.add('show-scroll-btn');
+//     } else {
+//         scrollToTopBtn.classList.remove('show-scroll-btn');
+//     }
+// });
+
+// scrollToTopBtn.addEventListener('click', () => {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+    // });
+// });
+
+});
